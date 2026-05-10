64bc0df389edd39b2f8d9877658c99c7856d3337
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-05-10 20:37:51 +0530

    and stock routes and server.js file

---
+++ server.js
@@ -0,0 +1,28 @@
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+const stockRoutes = require("./routes/stockRoutes");
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Stock Market Data Pipeline API Running");
+});
+
+app.use("/stocks", stockRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
+});
