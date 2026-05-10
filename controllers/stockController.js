9637842e9da8080453065e3d60f2a0ee6e1f4a77
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-05-10 20:36:17 +0530

    adding stock API controller using stock service

---
+++ controllers/stockController.js
@@ -0,0 +1,25 @@
+
+const { fetchStockData } = require("../services/stockService");
+
+const getStockData = async (req, res) => {
+  try {
+    const { symbol } = req.params;
+
+    const data = await fetchStockData(symbol);
+
+    res.status(200).json({
+      success: true,
+      symbol,
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  getStockData,
+};
