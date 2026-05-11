26a523de108ccbec6ef707d2f3292dfb5c4c82f2
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-05-11 14:19:01 +0530

    again fixed bug..mongodb atlas document wasnt getting updated

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -8,30 +8,42 @@ const getStockData = async (req, res) => {
     const { symbol } = req.params;
 
     const apiData = await fetchStockData(symbol);
+
+    // console.log(" API DATA:", apiData);
     const transformed = transformStockData(symbol, apiData);
+    // console.log(" TRANSFORMED:", transformed);
 
     if (!transformed) {
       return res.status(400).json({
         success: false,
-        message: "Invalid API response",
+        message: "No data to insert",
       });
     }
 
-    
-    try {
-      const stock = await Stock.create(transformed);
-    } catch (err) {
-      
-    }
+    // const stock = await Stock.create(transformed);
+    const stock = await Stock.findOneAndUpdate(
+  {
+    symbol: transformed.symbol,
+    timestamp: transformed.timestamp,
+  },
+  transformed,
+  {
+    upsert: true,  
+    new: true,      
+  }
+);
 
-
-    res.status(200).json({
+    // console.log(" DB INSERT SUCCESS:", stock);
+    return res.status(200).json({
       success: true,
       source: "api + db",
       data: transformed,
     });
+
   } catch (error) {
-    res.status(500).json({
+    console.log(" ERROR:", error.message);
+
+    return res.status(500).json({
       success: false,
       message: error.message,
     });
